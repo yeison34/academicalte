496f3f481513ea810c13189a20df1de00d373a9a
Marcela Guerrero <marcelaguerrero1396@gmail.com>
2023-05-28 16:42:07 -0500

    Creando un proyecto Laravel con AdminLTE

---
+++ resources/views/facultades/listado.blade.php
@@ -0,0 +1,24 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Facultades</h1>
+
+
+@stop
+
+@section('content')
+    <p>Listado de Facultades</p>
+
+
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
